76cd11eeaa3569af394f3231cf415874a3a0800f
seank2014 <shaun.king21@gmail.com>
2019-08-09 19:26:45 -0400

    flushing out shell

---
+++ Game/src/test/java/com/sg/game/TicTacLogicTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.game;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author seanking
+ */
+public class TicTacLogicTest {
+    
+    public TicTacLogicTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @org.junit.Test
+    public void testSomeMethod() {
+    }
+    
+}
