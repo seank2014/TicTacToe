998eeec4f99ba0e79e012ebc1b0a44ca7006600c
seank2014 <shaun.king21@gmail.com>
2019-08-09 19:41:43 -0400

    creating logic for computer to make plays

--- Game/src/main/java/com/sg/game/Computer.java
+++ Game/src/main/java/com/sg/game/Computer.java
@@ -10,5 +10,24 @@ package com.sg.game;
  * @author seanking
  */
 public class Computer {
-    
+
+    /*writing out the funtion that allows for the application also known
+    as the computer, to place its assigned marker on a spot when its turn
+    comes up*/
+    public int pickAspot(TicTacLogic game){
+        ArrayList <Integer> blocks = new ArrayList();
+        for(int i=0; 1 < 9; i++){
+            if(game.board[i] == ' '){
+                blocks.add(i + 1);
+            }
+        }
+
+    }
+
+/*This function allows for the marker to be placed randomly.
+With out this funtion, the computer would just place it's marker at the
+next available spot */
+    Random rand = new Random();
+    int block = blocks.get(Math.abs(rand.nextInt() % blocks.size()));
+    return block;
 }
