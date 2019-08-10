ded80510831acff2fa6005d4d7595b3a83b3714a
seank2014 <shaun.king21@gmail.com>
2019-08-10 00:44:08 -0400

    working out bugs

--- Game/src/main/java/com/sg/game/Computer.java
+++ Game/src/main/java/com/sg/game/Computer.java
@@ -12,31 +12,25 @@ import java.util.Random;
  *
  * @author seanking
  */
-
 public class Computer {
 
-    /*writing out the funtion that allows for the application also known
-    as the computer, to place its assigned marker on a spot when its turn
+    /*writing out the function that allows for the application also known
+    as the "computer", to place its assigned rep on a spot when its turn
     comes up*/
-    
-    public int pickAspot(TicTacLogic game){
+    public int pickAspot(TicTacLogic game) {
         ArrayList<Integer> blocks = new ArrayList();
-        for(int i=0; i < 9; i++){
-            if(game.board[i] == '-'){
+        for (int i = 0; i < 9; i++) {
+            if (game.board[i] == '-') {
                 blocks.add(i + 1);
             }
         }
-        
-    
-          
-                
-        /*This part allows for the marker to be placed randomly.
-With out this funtion, the computer would just place it's marker at the
+        /*This part allows for the rep to be placed randomly.
+With out this function, the computer would just place it's marker at the
 next available spot */
 
-    Random rand = new Random();
-    int block = blocks.get(Math.abs(rand.nextInt() % blocks.size()));
-    return block;
-}
+        Random rand = new Random();
+        int block = blocks.get(Math.abs(rand.nextInt() % blocks.size()));
+        return block;
+    }
 
 }
--- Game/src/main/java/com/sg/game/TicTacLogic.java
+++ Game/src/main/java/com/sg/game/TicTacLogic.java
@@ -10,183 +10,176 @@ package com.sg.game;
  * @author seanking
  */
 public class TicTacLogic {
-    
+
     //fields
-    protected char [] board;
+    protected char[] board;
     protected char userKey;
     protected char compKey;
     protected char winner;
     protected char currentKey;
-    
+
     //setting constructor
-    
-    
-   public TicTacLogic(char playerToken, char compKey){
-       this.userKey = playerToken;
-       this.compKey = compKey;
-       this.board = setBoard();
-       this.currentKey = userKey;
-       this.winner = ' ';
-       
-   }
-   
-   
-   public void printBoard(){
-       //Creating
-       //   | - | - | -
-       //   -------------
-      //    | - | - | -
-       //   -------------
-       //   | - | - | -
-       //   -------------
-       
-       System.out.println();
-       for(int i = 0; i < board.length; i++){
-           //divides the board into section of three
-           if(i % 3 == 0 && i != 0){
-               System.out.println();
-               System.out.println("-------------");
-           }
-           //will print divided and following "block"
-           System.out.print(" | " + board[i]);
-       }
-       System.out.println();
-   }
-   
-      public void printIndexBoard(){
-       System.out.println();
-       for(int i = 0; i < 9; i++){
-           if(i % 3 == 0 && i !=0){
-               System.out.println();
-               System.out.println("-------------");
-           }
-           System.out.print(" | " + (i + 1));
-       }
-          System.out.println();
-   }
-   
-   public char[] setBoard(){
-       //setting parameters of board
-       char[] board = new char[9];
-       for(int i = 0; i < board.length; i++){
-           board[i] = '-';
-       }
-       return board;
-   }
-   
-   //Check if spot selected in on board
-   public boolean inRange(int number){
-       return number > 0 && number < board.length + 1;
-   }
-   
-   //Check if spot selected is available
-   public boolean isSpotTaken(int number){
-       return board[number - 1] != '-';
-   }
-    
-   
-   //Check if it is users turn
-   public boolean playTurn(int spot){
-       boolean isValid = inRange(spot) && !isSpotTaken(spot);
-       if(isValid){
-           board[spot - 1] = currentKey;
-              currentKey = (currentKey == userKey) ? compKey : userKey;   
-          }
-          return isValid;
-       }
-   
-   public boolean isThereAWinner(){
-       boolean diagonalAndMiddleCol = (rightDi() || leftDi() || middleRow() || secondCol()) && board[4] != '-';
-       //   | - | - | -
-       //   -------------
-      //    | - | x | -
-       //   -------------
-       //   | - | - | -
-       //   -------------
-       
-       boolean topAndFirst = (topRow() || firstCol()) && board[0] != '-';
+    public TicTacLogic(char playerToken, char compKey) {
+        this.userKey = playerToken;
+        this.compKey = compKey;
+        this.board = setBoard();
+        this.currentKey = userKey;
+        this.winner = '-';
+
+    }
+
+    public void printBoard() {
+        //Creating
+        //   | - | - | -
+        //   -------------
+        //    | - | - | -
+        //   -------------
+        //   | - | - | -
+        //   -------------
+
+        System.out.println();
+        for (int i = 0; i < board.length; i++) {
+            //divides the board into section of three
+            if (i % 3 == 0 && i != 0) {
+                System.out.println();
+                System.out.println("-------------");
+            }
+            //will print divided and following "block"
+            System.out.print(" | " + board[i]);
+        }
+        System.out.println();
+    }
+
+    public static void printIndexBoard() {
+        System.out.println();
+        for (int i = 0; i < 9; i++) {
+            if (i % 3 == 0 && i != 0) {
+                System.out.println();
+                System.out.println("-------------");
+            }
+            System.out.print(" | " + (i + 1));
+        }
+        System.out.println();
+    }
+
+    public char[] setBoard() {
+        //setting parameters of board
+        char[] board = new char[9];
+        for (int i = 0; i < board.length; i++) {
+            board[i] = '-';
+        }
+        return board;
+    }
+
+    //Check if spot selected in on board
+    public boolean inRange(int number) {
+        return number > 0 && number < board.length + 1;
+    }
+
+    //Check if spot selected is available
+    public boolean isSpotTaken(int number) {
+        return board[number - 1] != '-';
+    }
+
+    //Check if it is users turn
+    public boolean playTurn(int spot) {
+        boolean isValid = inRange(spot) && !isSpotTaken(spot);
+        if (isValid) {
+            board[spot - 1] = currentKey;
+            currentKey = (currentKey == userKey) ? compKey : userKey;
+        }
+        return isValid;
+    }
+
+    public boolean isThereAWinner() {
+        boolean diagonalAndMiddleCol = (rightDi() || leftDi() || middleRow() || secondCol()) && board[4] != '-';
+        //   | - | - | -
+        //   -------------
+        //    | - | x | -
+        //   -------------
+        //   | - | - | -
+        //   -------------
+
+        boolean topAndFirst = (topRow() || firstCol()) && board[0] != '-';
         //  | x | - | -
-       //   -------------
-      //    | - | - | -
-       //   -------------
-       //   | - | - | -
-       //   -------------
-       
-       boolean bottomAndThird = (bottomRow() || thirdCol()) && board[8] != '-';
-       //   | - | - | -
-       //   -------------
-      //    | - | - | -
-       //   -------------
-       //   | - | - | x
-       //   -------------
-       
-       if(diagonalAndMiddleCol){
-           System.out.println("Won diagonally/Middle");
-           this.winner = board[4];
-       }else if(topAndFirst){
-           System.out.println("Won from the top/first column");
-           this.winner = board[0];
-       } else if(bottomAndThird){
-           System.out.println("Won from the last row/column");
-           this.winner = board[8];
-       }
-       return diagonalAndMiddleCol || topAndFirst || bottomAndThird;
-   }
-   
-   public boolean rightDi(){
-       return board[2] == board[4] && board[4] == board[6];
-   }
-   
-   public boolean leftDi(){
-       return board[0] == board[4] && board[4] == board[8];
-   }
-   
-   public boolean middleRow(){
-       return board[3] == board[4] && board[4] == board[5];
-   }
-   
-   public boolean secondCol(){
-       return board[1] == board[4] && board[4] == board[7];
-   }
-   
-   
-   public boolean topRow(){
-       return board[0] == board[1] && board[1] == board[2];
-   }
-   
-   public boolean firstCol(){
-       return board[0] == board[3] && board[3] == board[6];
-   }
-   
-   public boolean bottomRow(){
-       return board[6] == board[7] && board[7] == board[8];
-   }
-   
-   public boolean thirdCol(){
-       return board[2] == board[5] && board[5] == board[8];
-   }
-   
-  public boolean anyPlaysLeft(){
-       for(int i=0; i > board.length; i++){
-           if(board[i] =='-'){
-               return false;
-           }
-       }
-       return true;
-   }
-   
-  public String gameOver(){
-      boolean didSomeoneWin = isThereAWinner();
-      if(didSomeoneWin){
-          return "The winner is " + this.winner + " 's"; 
-      } else if(anyPlaysLeft()){
-          return "Draw: Game Over!";
-          
-      } else{
-          return "Game is not over";
-      }
-  }
-   
-   
+        //   -------------
+        //    | - | - | -
+        //   -------------
+        //   | - | - | -
+        //   -------------
+
+        boolean bottomAndThird = (bottomRow() || thirdCol()) && board[8] != '-';
+        //   | - | - | -
+        //   -------------
+        //    | - | - | -
+        //   -------------
+        //   | - | - | x
+        //   -------------
+
+        if (diagonalAndMiddleCol) {
+            System.out.println("Won diagonally/Middle");
+            this.winner = board[4];
+        } else if (topAndFirst) {
+            System.out.println("Won from the top/first column");
+            this.winner = board[0];
+        } else if (bottomAndThird) {
+            System.out.println("Won from the last row/column");
+            this.winner = board[8];
+        }
+        return diagonalAndMiddleCol || topAndFirst || bottomAndThird;
+    }
+
+    public boolean rightDi() {
+        return board[2] == board[4] && board[4] == board[6];
+    }
+
+    public boolean leftDi() {
+        return board[0] == board[4] && board[4] == board[8];
+    }
+
+    public boolean middleRow() {
+        return board[3] == board[4] && board[4] == board[5];
+    }
+
+    public boolean secondCol() {
+        return board[1] == board[4] && board[4] == board[7];
+    }
+
+    public boolean topRow() {
+        return board[0] == board[1] && board[1] == board[2];
+    }
+
+    public boolean firstCol() {
+        return board[0] == board[3] && board[3] == board[6];
+    }
+
+    public boolean bottomRow() {
+        return board[6] == board[7] && board[7] == board[8];
+    }
+
+    public boolean thirdCol() {
+        return board[2] == board[5] && board[5] == board[8];
+    }
+
+    public boolean anyPlaysLeft() {
+        for (int i = 0; i < board.length; i++) {
+            if (board[i] == '-') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String gameOver() {
+        boolean didSomeoneWin = isThereAWinner();
+        if (didSomeoneWin) {
+            return "The winner is " + this.winner + " 's";
+        } else if (anyPlaysLeft()) {
+            return "Draw: Game Over!";
+
+        } else {
+            return "Game is not over";
+        }
+    }
 
 }
