9b57ef06f33b91e8615e9ae515ddb2db800ffaed
seank2014 <shaun.king21@gmail.com>
2019-08-09 20:43:25 -0400

    printing board

--- Game/src/main/java/com/sg/game/Computer.java
+++ Game/src/main/java/com/sg/game/Computer.java
@@ -22,13 +22,15 @@ public class Computer {
     public int pickAspot(TicTacLogic game){
         ArrayList<Integer> blocks = new ArrayList();
         for(int i=0; 1 < 9; i++){
-            if(game.board[i] == ' '){
+            if(game.board[i] == '-'){
                 blocks.add(i + 1);
             }
         }
-/*This part allows for the marker to be placed randomly.
+        
+        /*This part allows for the marker to be placed randomly.
 With out this funtion, the computer would just place it's marker at the
 next available spot */
+
     Random rand = new Random();
     int block = blocks.get(Math.abs(rand.nextInt() % blocks.size()));
     return block;
--- Game/src/main/java/com/sg/game/TicTacLogic.java
+++ Game/src/main/java/com/sg/game/TicTacLogic.java
@@ -11,4 +11,45 @@ package com.sg.game;
  */
 public class TicTacLogic {
     
+    //fields
+    protected char [] board;
+    protected char userKey;
+    protected char compKey;
+    protected char winner;
+    protected char currentKey;
+    
+    //setting constructor
+    
+    
+   public TicTacLogic(char playerToken, char compKey){
+       this.userKey = playerToken;
+       this.compKey = compKey;
+       this.board = setBoard();
+       this.currentKey = userKey;
+       this.winner = ' ';
+       
+   }
+   
+   public void printBoard(){
+       //Creating
+       //   | - | - | -
+       //   -------------
+      //    | - | - | -
+       //   -------------
+       //   | - | - | -
+       //   -------------
+       
+       System.out.println();
+       for(int i = 0; i < board.length; i++){
+           //divides the board into section of three
+           if(i % 3 == 0 && i != 0){
+               System.out.println();
+               System.out.println("-------------");
+           }
+           //will print divided and following "block"
+           System.out.println(" | " + board[i]);
+       }
+       System.out.println();
+   }
+    
 }
